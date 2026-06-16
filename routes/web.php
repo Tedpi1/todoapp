8fe63bc9dbeb1c677cac6db037b0c4ea9c736a31
Tedpi1 <moroted16@gmail.com>
2026-06-16 22:24:31 +0300

    login in process:

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthManager;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthManager::class, 'login'])->name('login');
